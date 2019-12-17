0c7c2315808848980992fbe7790a1722933a3e78
linckye <linckye@gmail.com>
2019-12-17 16:02:27 +0800

    12.17.16:02

--- src/main/java/com/imooc/demospring/ye/IO.java
+++ src/main/java/com/imooc/demospring/ye/IO.java
@@ -1,6 +1,8 @@
 package com.imooc.demospring.ye;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * @author linckye
@@ -9,14 +11,33 @@ public class IO {
     public static void main(String[] args) {
         try{
             System.out.println("please input");
-            Integer b;
-            b = System.in.read();
-            while(b != -1){
-                System.out.println(b);
+            int b;
+            while((b = System.in.read()) != -1){
+                System.out.println((char) b);
             }
         }catch (IOException e){
             System.out.println(e.toString());
         }
-
     }
+//public static void main(String args[]) {
+//    String s;
+//    // 创建缓冲区阅读器从键盘逐行读入数据
+//    InputStreamReader ir = new InputStreamReader(System.in);
+//    BufferedReader in = new BufferedReader(ir);
+//    System.out.println("Unix系统: ctrl-d 或 ctrl-c 退出"
+//            + "\nWindows系统: ctrl-z 退出");
+//    try {
+//        // 读一行数据，并标准输出至显示器
+//        s = in.readLine();
+//        // readLine()方法运行时若发生I/O错误，将抛出IOException异常
+//        while (s != null) {
+//            System.out.println("Read: " + s);
+//            s = in.readLine();
+//        }
+//        // 关闭缓冲阅读器
+//        in.close();
+//    } catch (IOException e) { // Catch any IO exceptions.
+//        e.printStackTrace();
+//    }
+//}
 }
